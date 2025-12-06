9a1d27a8a33bdd0f8e03dbefc42490af39805e67
Steven Bailey <sraybailey@gmail.com>
2025-12-06 08:58:33 -0500

    Day 6 Problem 1 (PowerShell)

---
+++ Day 6/PowerShell/Problem 1.ps1
@@ -0,0 +1,27 @@
+﻿function SolveProblem1 {
+	[CmdletBinding()]
+	[OutputType([int])]
+	param([string]$InputPath)
+
+	$inputData = Get-Content -Path $InputPath
+	$workableData = $inputData -split "`n"
+
+	$operands = $workableData[-1].Trim() -split "\s+"
+
+	$answer = $workableData[0].Trim() -split "\s+"
+
+	foreach ($line in 1..($workableData.Count - 2)) {
+		$lineNumbers = $workableData[$line].Trim() -split "\s+"
+		Write-Debug "Line numbers: $($lineNumbers -join ', ')"
+		foreach ($entry in 0..($lineNumbers.Count - 1)) {
+			$mathProblem = "$($answer[$entry]) $($operands[$entry]) $($lineNumbers[$entry])"
+			Write-Verbose "Executing: $mathProblem"
+			$scriptBlock = [scriptblock]::Create($mathProblem)
+			$answer[$entry] = & $scriptBlock
+			Write-Debug "$mathProblem = $($answer[$entry])"
+		}
+		Write-Debug "Answers at this stage: $($answer -join ', ')"
+	}
+
+	return ($answer | Measure-Object -Sum).Sum
+}
